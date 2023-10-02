eaff60b54e8e45653012e4a4f55dd4507e5203ac
Sam <samdc@apami.com>
2023-10-02 19:49:29 +0100

    Initial creation of management sdk

---
+++ lib/mockzilla-management/src/commonTest/kotlin/TestTest.kt
@@ -0,0 +1,10 @@
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class TestTest {
+
+    @Test
+    fun test() {
+        assertTrue { true }
+    }
+}
--- lib/settings.gradle.kts
+++ lib/settings.gradle.kts
@@ -16,3 +16,4 @@ dependencyResolutionManagement {
 rootProject.name = "lib"
 includeBuild("build-logic")
 include(":mockzilla")
+include(":mockzilla-management")
