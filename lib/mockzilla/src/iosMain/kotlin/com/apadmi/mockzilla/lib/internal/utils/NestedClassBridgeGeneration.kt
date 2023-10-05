0c2fdc5127e4ff7179b4303b7ba2a08ebd8a3076
Sam <samdc@apami.com>
2023-10-05 12:50:26 +0100

    çhore: Keep spotless happy

--- lib/mockzilla/src/iosMain/kotlin/com/apadmi/mockzilla/lib/internal/utils/NestedClassBridgeGeneration.kt
+++ lib/mockzilla/src/iosMain/kotlin/com/apadmi/mockzilla/lib/internal/utils/NestedClassBridgeGeneration.kt
@@ -6,5 +6,8 @@ import com.apadmi.mockzilla.lib.models.MockzillaConfig
  * For some infuriating reason, nested classes defined in dependent modules don't automatically have
  * headers generated unless they're being used within the module. This usage forces the generation
  * of the headers.
+ *
+ * @param noop [MockzillaConfig.Builder] Just a reference to the builder to force the header generation.
+ * @return
  */
 fun noOpConfigBuilder(noop: MockzillaConfig.Builder) = Unit
