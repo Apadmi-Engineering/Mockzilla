b25ac4b315bc0e974d41a4cf79ce9884a47e798b
Skeletonxf <skeletonxf@gmail.com>
2023-10-20 16:20:42 +0100

    Add previews and make Add Device tab selection work properly on tab bar UI again

--- lib/mockzilla-management-ui/common/src/commonMain/kotlin/com/apadmi/mockzilla/desktop/ui/widgets/devicetabs/DeviceTabsWidget.kt
+++ lib/mockzilla-management-ui/common/src/commonMain/kotlin/com/apadmi/mockzilla/desktop/ui/widgets/devicetabs/DeviceTabsWidget.kt
@@ -1,15 +1,18 @@
 package com.apadmi.mockzilla.desktop.ui.widgets.devicetabs
 
+import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import com.airbnb.android.showkase.annotation.ShowkaseComposable
 
 import com.apadmi.mockzilla.desktop.di.utils.getViewModel
+import com.apadmi.mockzilla.desktop.engine.device.Device
 import com.apadmi.mockzilla.desktop.i18n.LocalStrings
 import com.apadmi.mockzilla.desktop.i18n.Strings
+import com.apadmi.mockzilla.desktop.ui.components.PreviewSurface
 import com.apadmi.mockzilla.desktop.ui.scaffold.HorizontalTab
 import com.apadmi.mockzilla.desktop.ui.scaffold.HorizontalTabList
 import com.apadmi.mockzilla.desktop.ui.widgets.devicetabs.DeviceTabsViewModel.*
@@ -37,10 +40,17 @@ fun DeviceTabsWidgetContent(
     onSelect: (State.DeviceTabEntry) -> Unit,
     onAddNewDevice: () -> Unit
 ) {
+    val selectedDevice = state.devices.indexOfFirst { it.isActive }
+    val selectedTab = if (selectedDevice == -1) {
+        state.devices.lastIndex + 1
+    } else {
+        selectedDevice
+    }
     Surface(modifier = modifier) {
         HorizontalTabList(
-            tabs = state.devices.map { HorizontalTab(title = it.toString()) } + HorizontalTab(strings.widgets.deviceTabs.addDevice),
-            selected = state.devices.indexOfFirst { it.isActive },
+            tabs = state.devices.map { HorizontalTab(title = it.toString()) } +
+                    HorizontalTab(strings.widgets.deviceTabs.addDevice),
+            selected = selectedTab,
             onSelect = {
                 if (it > state.devices.lastIndex) {
                     onAddNewDevice()
@@ -50,4 +60,35 @@ fun DeviceTabsWidgetContent(
             },
         )
     }
+}
+
+@ShowkaseComposable("DeviceTabsWidget-NoDeviceConnected", "DeviceTabsWidget")
+@Composable
+@Preview
+fun DeviceTabsWidgetEmptyContentPreview() = PreviewSurface {
+    DeviceTabsWidgetContent(
+        State(devices = listOf()),
+        onSelect = {},
+        onAddNewDevice = {},
+    )
+}
+
+@ShowkaseComposable("DeviceTabsWidget-OneDeviceConnected", "DeviceTabsWidget")
+@Composable
+@Preview
+fun DeviceTabsWidgetContentPreview() = PreviewSurface {
+    DeviceTabsWidgetContent(
+        State(
+            devices = listOf(
+                State.DeviceTabEntry(
+                    name = "Phone",
+                    isActive = true,
+                    isConnected = true,
+                    underlyingDevice = Device(ip = "123.456.123.456", port = "8080")
+                )
+            )
+        ),
+        onSelect = {},
+        onAddNewDevice = {},
+    )
 }
