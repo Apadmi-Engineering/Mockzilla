bc2808f47c9bb82fac23ff2c2d68a81ed75e119e
Tom Handcock <33254448+TomRHandcock@users.noreply.github.com>
2023-10-20 21:36:32 +0100

    revert: Remove `MethodChannelMockzillaPlatformInterface`

--- lib/FlutterMockzilla/mockzilla_platform_interface/lib/mockzilla_method_channel.dart
+++
@@ -1,6 +0,0 @@
-import 'package:mockzilla_platform_interface/mockzilla_platform_interface.dart';
-
-/// An implementation of [MockzillaPlatformInterfacePlatform] that uses method channels.
-class MethodChannelMockzillaPlatformInterface extends MockzillaPlatform {
-
-}
