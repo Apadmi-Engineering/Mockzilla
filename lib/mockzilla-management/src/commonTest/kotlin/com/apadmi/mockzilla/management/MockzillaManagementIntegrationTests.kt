5cf72b371525603ead083539e74e09275e48248f
JordanH <jordanh@apadmi.com>
2023-10-23 17:23:31 +0100

    Write get tests

--- lib/mockzilla-management/src/commonTest/kotlin/com/apadmi/mockzilla/management/MockzillaManagementIntegrationTests.kt
+++ lib/mockzilla-management/src/commonTest/kotlin/com/apadmi/mockzilla/management/MockzillaManagementIntegrationTests.kt
@@ -1,15 +1,18 @@
 package com.apadmi.mockzilla.management
 
+import com.apadmi.mockzilla.lib.internal.models.LogEvent
 import com.apadmi.mockzilla.lib.internal.models.MonitorLogsResponse
+import com.apadmi.mockzilla.lib.internal.models.toJson
+import com.apadmi.mockzilla.lib.internal.utils.JsonProvider
 import com.apadmi.mockzilla.lib.models.EndpointConfiguration
 import com.apadmi.mockzilla.lib.models.MetaData
 import com.apadmi.mockzilla.lib.models.MockzillaConfig
 import com.apadmi.mockzilla.lib.models.MockzillaHttpResponse
 import com.apadmi.mockzilla.testutils.runIntegrationTest
-import io.ktor.client.utils.HttpRequestCreated
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.request.get
 import io.ktor.http.HttpStatusCode
-import testutils.dummyEmpty
-
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -48,53 +51,78 @@ class MockzillaManagementIntegrationTests {
             /* Verify */
             assertEquals(
                 expected = Result.success(
-                    MonitorLogsResponse.dummyEmpty()
+                    MonitorLogsResponse(
+                        appPackage = "-", logs = emptyList()
+                    )
                 ),
                 actual = result
             )
         }
 
+
+    private val fetchLogsAndClearBufferEndpoint = "clear-endpoint"
+
+    //the mock log
+    private val mockLog = LogEvent(
+        timestamp = 0,
+        url = "/local-mock/$fetchLogsAndClearBufferEndpoint",
+        requestBody = "",
+        requestHeaders = emptyMap(),
+        responseBody = "",
+        responseHeaders = emptyMap(),
+        status = HttpStatusCode.Created,
+        delay = 24,
+        method = "GET",
+        isIntendedFailure = false
+    )
+
+
     @Test
-    fun `fetchMonitorLogsAndClearBuffer with network calls- returns Monitor Logs`() =
-
-
+    fun `fetchMonitorLogsAndClearBuffer with network calls- returns list of logs`() =
         runIntegrationTest(
             dummyAppName,
             dummyAppVersion,
             config = MockzillaConfig.Builder()
-                .setPort(0)
+                .setPort(0)  // Port determined at runtime
                 .setFailureProbabilityPercentage(0)
                 .setMeanDelayMillis(24)
                 .setDelayVarianceMillis(0)
                 .addEndpoint(
-                    endpoint = EndpointConfiguration.Builder("my-id")
+                    EndpointConfiguration.Builder(fetchLogsAndClearBufferEndpoint)
                         .setDefaultHandler {
                             MockzillaHttpResponse(
                                 statusCode = HttpStatusCode.Created,
-                                headers = mapOf("test" to "another test"),
-                                body = "body"
+                                body = ""
                             )
                         }.build()
-                ).build()
-        ) { sut, connection, _ ->
+                )
+                .build()
+        ) { sut, connection, params ->
 
             /* Setup */
 
+            // Make a request for network logs
+            HttpClient(CIO)
+                .get(urlString = "${params.mockBaseUrl}/$fetchLogsAndClearBufferEndpoint")
 
 
-
-
+            val expectedLogs = MonitorLogsResponse(appPackage = "-", listOf(mockLog))
 
             /* Run Test */
-            val result = sut.fetchMonitorLogsAndClearBuffer(connection)
+            val actualLogs = sut.fetchMonitorLogsAndClearBuffer(connection).getOrNull()!!
 
             /* Verify */
             assertEquals(
-                expected = Result.success(
-                    MonitorLogsResponse.dummyEmpty()
-                ),
-                actual = result
+                expected = expectedLogs,
+                actual = actualLogs.copy(logs = actualLogs.logs.map {
+                    it.copy(
+                        timestamp = 0,
+                        requestHeaders = emptyMap()
+                    )
+                })
             )
 
+
         }
+
 }
