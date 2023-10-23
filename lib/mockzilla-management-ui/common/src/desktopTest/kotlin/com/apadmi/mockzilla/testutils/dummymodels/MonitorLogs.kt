b9682fb88f4727ca01c871de093c7adcd6d2edce
JordanH <jordanh@apadmi.com>
2023-10-23 13:26:26 +0100

    Create mock MonitorLogsResponse

--- lib/mockzilla-management-ui/common/src/desktopTest/kotlin/com/apadmi/mockzilla/testutils/dummymodels/MonitorLogs.kt
+++ lib/mockzilla-management-ui/common/src/desktopTest/kotlin/com/apadmi/mockzilla/testutils/dummymodels/MonitorLogs.kt
@@ -1,4 +1,27 @@
 package com.apadmi.mockzilla.testutils.dummymodels
 
-class MonitorLogs {
-}
+import com.apadmi.mockzilla.lib.internal.models.LogEvent
+import com.apadmi.mockzilla.lib.internal.models.MonitorLogsResponse
+import io.ktor.http.HttpStatusCode
+
+
+fun MonitorLogsResponse.Companion.dummy()  = MonitorLogsResponse(
+    appPackage = "package",
+    logs = listOf(
+        LogEvent(
+            timestamp = 1234,
+            url = "url",
+            requestBody = "body",
+            requestHeaders = mapOf("1" to "request1", "2" to "request2"),
+            responseHeaders = mapOf("1" to "response1", "2" to "request2"),
+            responseBody = "body",
+            status = HttpStatusCode.Accepted,
+            delay = 1000,
+            method = "method",
+            isIntendedFailure = false,
+
+        )
+
+
+    )
+)
