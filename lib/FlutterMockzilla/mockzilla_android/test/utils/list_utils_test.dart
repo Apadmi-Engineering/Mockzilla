950dc3e9b92c354a366ea7f0e0dcac4c179684f9
Tom Handcock <33254448+TomRHandcock@users.noreply.github.com>
2023-12-23 12:46:41 +0000

    test: Add unit test for list utils

---
+++ lib/FlutterMockzilla/mockzilla_android/test/utils/list_utils_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockzilla_android/src/utils/list_utils.dart';
+
+void main() {
+  group("List Utils unit tests", () {
+
+    test("firstWhereOrNull - returns expected result", () {
+      // Setup
+      final inputToExpected = {
+        [0, 1, 2]: null,
+        [0, 1, 3]: 3,
+        []: null,
+        [3]: 3,
+        [1, 2, 3, 4]: 3
+      };
+
+      // Run test & verify
+      inputToExpected.forEach((input, expected) {
+        expect(input.firstWhereOrNull((element) => element >= 3), expected);
+      });
+    });
+  });
+}
