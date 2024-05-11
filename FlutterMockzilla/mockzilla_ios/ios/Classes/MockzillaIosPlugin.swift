f5c881ca3868f4b373963b29b176d9d0fbf5fa53
Tom Handcock <tomh@apadmi.com>
2024-05-11 22:00:15 +0100

    chore: Register `MockzillaIos` as platform channel handler in `mockzilla_ios`

--- FlutterMockzilla/mockzilla_ios/ios/Classes/MockzillaIosPlugin.swift
+++ FlutterMockzilla/mockzilla_ios/ios/Classes/MockzillaIosPlugin.swift
@@ -3,18 +3,9 @@ import UIKit
 import mockzilla
 
 public class MockzillaIosPlugin: NSObject, FlutterPlugin {
+    
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "mockzilla_ios", binaryMessenger: registrar.messenger())
-    let instance = MockzillaIosPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
-    }
+    let plugin = MockzillaIos()
+      MockzillaHostApiSetup.setUp(binaryMessenger: registrar.messenger(), api: plugin)
   }
 }
