127a898de9a807051a5488fe63814c2560bbf6e5
Tom Handcock <tomh@apadmi.com>
2024-05-11 21:52:43 +0100

    feat: Implement `startServer` + `stopServer` in `mockzilla_ios`

--- FlutterMockzilla/mockzilla_ios/ios/Classes/MockzillaIos.swift
+++ FlutterMockzilla/mockzilla_ios/ios/Classes/MockzillaIos.swift
@@ -6,18 +6,23 @@
 //
 
 import Foundation
+import Flutter
 import mockzilla
 
 extension FlutterError: Error {}
 
 class MockzillaIos: MockzillaHostApi {
     func startServer(config: BridgeMockzillaConfig) throws -> BridgeMockzillaRuntimeParams {
-        
+        let params = MockzillaKt.startMockzilla(
+            config: config.toNative(endpointMatcher: { key, request in true },
+                                    defaultHandler: { key, request in Mockzilla_commonMockzillaHttpResponse(statusCode: Ktor_httpHttpStatusCode(value: 200, description: "Ok"), headers: [:], body: "")},
+                                    errorHandler: { key, request in Mockzilla_commonMockzillaHttpResponse(statusCode: Ktor_httpHttpStatusCode(value: 500, description: "Internal error"), headers: [:], body: "")}
+                                   )
+        )
+        return try BridgeMockzillaRuntimeParams.fromNative(params)
     }
     
     func stopServer() throws {
-        <#code#>
+        MockzillaKt.stopMockzilla()
     }
-    
-    
 }
