0bb4d89b2ce872d0613cf0853df130b3cf9e4950
Sam <samdc@apami.com>
2024-05-13 13:27:58 +0100

    fix: Fix paparazzi dependency issues

--- mockzilla-management-ui/common/build.gradle.kts
+++ mockzilla-management-ui/common/build.gradle.kts
@@ -99,6 +99,22 @@ dependencies {
         }
 }
 
+// TODO Remove when https://github.com/google/guava/issues/6567 is fixed.
+// See also: https://github.com/google/guava/issues/6801.
+// Paparazzi issue here: https://github.com/cashapp/paparazzi/issues/1231
+dependencies.constraints {
+    testImplementation("com.google.guava:guava") {
+        attributes {
+            attribute(
+                TargetJvmEnvironment.TARGET_JVM_ENVIRONMENT_ATTRIBUTE,
+                objects.named(TargetJvmEnvironment::class.java, TargetJvmEnvironment.STANDARD_JVM)
+            )
+        }
+        because("Paparazzi's layoutlib and sdk-common depend on Guava's -jre published variant." +
+                "See https://github.com/cashapp/paparazzi/issues/906.")
+    }
+}
+
 android {
     namespace = group.toString()
     compileSdk = AndroidConfig.targetSdk
