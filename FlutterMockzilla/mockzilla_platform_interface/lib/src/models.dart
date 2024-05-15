44297e63b050ea68e3266347f08a2ee2a37edbd6
JordanH <jordanh@apadmi.com>
2024-05-15 20:26:11 +0100

    feat: implement builder with new builder steps

--- FlutterMockzilla/mockzilla_platform_interface/lib/src/models.dart
+++ FlutterMockzilla/mockzilla_platform_interface/lib/src/models.dart
@@ -46,21 +46,19 @@ class MockzillaHttpResponse with _$MockzillaHttpResponse {
   }) = _MockzillaHttpResponse;
 }
 
-abstract class EndpointBuilderBuildStep
-    implements EndpointBuilderOptionalsStep {
-  EndpointConfig build();
-}
-
 abstract class EndpointBuilderDefaultHandlerStep {
-  EndpointBuilderMatcherStep setDefaultHandler();
+  EndpointBuilderMatcherStep setDefaultHandler(
+      MockzillaHttpResponse Function(MockzillaHttpRequest) defaultHandler);
 }
 
 abstract class EndpointBuilderMatcherStep {
-  EndpointBuilderErrorHandlerStep setEndpointMatcher();
+  EndpointBuilderErrorHandlerStep setEndpointMatcher(
+      bool Function(MockzillaHttpRequest) endpointMatcher);
 }
 
 abstract class EndpointBuilderErrorHandlerStep {
-  EndpointBuilderOptionalsStep setErrorHandler();
+  EndpointBuilderOptionalsStep setErrorHandler(
+      MockzillaHttpResponse Function(MockzillaHttpRequest) errorHandler);
 }
 
 abstract class EndpointBuilderOptionalsStep {
@@ -71,16 +69,29 @@ abstract class EndpointBuilderOptionalsStep {
   EndpointBuilderBuildStep setDelayMean(int delayVariance);
 }
 
-class EndpointConfigBuilder {
+abstract class EndpointBuilderBuildStep
+    implements EndpointBuilderOptionalsStep {
+  EndpointConfig build();
+}
+
+class EndpointConfigBuilder
+    implements
+        EndpointBuilderDefaultHandlerStep,
+        EndpointBuilderMatcherStep,
+        EndpointBuilderErrorHandlerStep,
+        EndpointBuilderOptionalsStep,
+        EndpointBuilderBuildStep {
   final String name;
 
   final String key;
 
-  final bool Function(MockzillaHttpRequest) endpointMatcher;
+  bool Function(MockzillaHttpRequest) endpointMatcher = (_) => false;
 
-  final MockzillaHttpResponse Function(MockzillaHttpRequest) defaultHandler;
+  MockzillaHttpResponse Function(MockzillaHttpRequest) defaultHandler =
+      (_) => const MockzillaHttpResponse();
 
-  final MockzillaHttpResponse Function(MockzillaHttpRequest) errorHandler;
+  MockzillaHttpResponse Function(MockzillaHttpRequest) errorHandler =
+      (_) => const MockzillaHttpResponse();
 
   int? failureProbability;
   int? delayMean;
@@ -89,26 +100,27 @@ class EndpointConfigBuilder {
   EndpointConfigBuilder({
     required this.name,
     required this.key,
-    required this.endpointMatcher,
-    required this.defaultHandler,
-    required this.errorHandler,
   });
 
+  @override
   EndpointConfigBuilder setFailureProbability(int failureProbability) {
     this.failureProbability = failureProbability;
     return this;
   }
 
+  @override
   EndpointConfigBuilder setMeanDelayMillis(int delayMean) {
     this.delayMean = delayMean;
     return this;
   }
 
-  EndpointConfigBuilder withDelayMean(int delayVariance) {
+  @override
+  EndpointConfigBuilder setDelayMean(int delayVariance) {
     this.delayVariance = delayVariance;
     return this;
   }
 
+  @override
   EndpointConfig build() {
     return EndpointConfig(
       key: key,
@@ -117,6 +129,27 @@ class EndpointConfigBuilder {
       defaultHandler: defaultHandler,
       errorHandler: errorHandler,
     );
+  }
+
+  @override
+  EndpointBuilderMatcherStep setDefaultHandler(
+      MockzillaHttpResponse Function(MockzillaHttpRequest) defaultHandler) {
+    this.defaultHandler = defaultHandler;
+    return this;
+  }
+
+  @override
+  EndpointBuilderErrorHandlerStep setEndpointMatcher(
+      bool Function(MockzillaHttpRequest) endpointMatcher) {
+    this.endpointMatcher = endpointMatcher;
+    return this;
+  }
+
+  @override
+  EndpointBuilderOptionalsStep setErrorHandler(
+      MockzillaHttpResponse Function(MockzillaHttpRequest) errorHandler) {
+    this.errorHandler = errorHandler;
+    return this;
   }
 }
 
@@ -163,9 +196,14 @@ class EndpointConfig with _$EndpointConfig {
         errorHandler,
   }) = _EndpointConfig;
 
-  static EndpointBuilderDefaultHandlerStep builder(String name) {
-    throw UnimplementedError();
-  }
+  static EndpointBuilderDefaultHandlerStep builder(
+    String name,
+    String key,
+  ) =>
+      EndpointConfigBuilder(
+        name: name,
+        key: key,
+      );
 }
 
 @freezed
