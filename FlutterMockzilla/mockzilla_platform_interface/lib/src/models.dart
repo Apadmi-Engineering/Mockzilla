bea825fef2b6766d929e63583a5002e388bd412b
JordanH <jordanh@apadmi.com>
2024-05-15 19:54:36 +0100

    feat: add endpoint configuration builder

--- FlutterMockzilla/mockzilla_platform_interface/lib/src/models.dart
+++ FlutterMockzilla/mockzilla_platform_interface/lib/src/models.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:mockzilla_platform_interface/mockzilla_platform_interface.dart';
 
 part 'models.freezed.dart';
 
@@ -46,6 +47,55 @@ class MockzillaHttpResponse with _$MockzillaHttpResponse {
 }
 
 // TODO: Implement builder pattern to create endpoints.
+class EndpointConfigBuilder {
+  final String name;
+
+  final String key;
+
+  final bool Function(MockzillaHttpRequest) endpointMatcher;
+
+  final MockzillaHttpResponse Function(MockzillaHttpRequest) defaultHandler;
+
+  final MockzillaHttpResponse Function(MockzillaHttpRequest) errorHandler;
+
+  int? failureProbability;
+  int? delayMean;
+  int? delayVariance;
+
+  EndpointConfigBuilder({
+    required this.name,
+    required this.key,
+    required this.endpointMatcher,
+    required this.defaultHandler,
+    required this.errorHandler,
+  });
+
+  EndpointConfigBuilder setFailureProbability(int failureProbability) {
+    this.failureProbability = failureProbability;
+    return this;
+  }
+
+  EndpointConfigBuilder setMeanDelayMillis(int delayMean) {
+    this.delayMean = delayMean;
+    return this;
+  }
+
+  EndpointConfigBuilder withDelayMean(int delayVariance) {
+    this.delayVariance = delayVariance;
+    return this;
+  }
+
+  EndpointConfig build() {
+    return EndpointConfig(
+      key: key,
+      name: name,
+      endpointMatcher: endpointMatcher,
+      defaultHandler: defaultHandler,
+      errorHandler: errorHandler,
+    );
+  }
+}
+
 @freezed
 class EndpointConfig with _$EndpointConfig {
   /// This configuration defines how Mockzilla should deal with a subset of
@@ -57,32 +107,54 @@ class EndpointConfig with _$EndpointConfig {
   const factory EndpointConfig({
     required String name,
     required String key,
+
     /// Probability as a percentage that the Mockzilla server should return an
     /// error for any single request to this endpoint.
     int? failureProbability,
+
     /// Optional, the artificial delay in milliseconds that Mockzilla should use to
     /// simulate latency.
     int? delayMean,
+
     /// Optional, the variance in milliseconds of the artificial delay applied
     /// by Mockzilla to a response to simulate latency. If not provided, then a
     /// default of 0ms is used to eliminate randomness.
     int? delayVariance,
+
     /// Used to determine whether a particular `request` should be evaluated by
     /// this endpoint.
     required bool Function(MockzillaHttpRequest request) endpointMatcher,
     MockzillaHttpResponse? webApiDefaultResponse,
-
     MockzillaHttpResponse? webApiErrorResponse,
+
     /// This function is called when a network request is made to this endpoint,
     /// note that if an error is being returned due to `failureProbability`
     /// then `errorHandler` is used instead.
     required MockzillaHttpResponse Function(MockzillaHttpRequest request)
         defaultHandler,
+
     /// This function is called when, in response to a network request, the
     /// server returns an error due to`failureProbability`.
     required MockzillaHttpResponse Function(MockzillaHttpRequest request)
         errorHandler,
   }) = _EndpointConfig;
+
+  static EndpointConfigBuilder builder({
+    required String name,
+    required String key,
+    required bool Function(MockzillaHttpRequest) endpointMatcher,
+    required MockzillaHttpResponse Function(MockzillaHttpRequest)
+        defaultHandler,
+    required MockzillaHttpResponse Function(MockzillaHttpRequest) errorHandler,
+  }) {
+    return EndpointConfigBuilder(
+      name: name,
+      key: key,
+      endpointMatcher: endpointMatcher,
+      defaultHandler: defaultHandler,
+      errorHandler: errorHandler,
+    );
+  }
 }
 
 @freezed
@@ -106,12 +178,15 @@ class MockzillaConfig with _$MockzillaConfig {
   const factory MockzillaConfig({
     /// The port that the Mockzilla should be available through.
     required int port,
+
     /// The list of available mocked endpoints.
     required List<EndpointConfig> endpoints,
+
     /// Can be used to add rudimentary restrictions to the Mockzilla server
     /// such as rate limiting. See [https://apadmi-engineering.github.io/Mockzilla/additional_config/#release-mode]()
     /// for more information.
     required bool isRelease,
+
     /// Whether Mockzilla server should only be available on the host device.
     required bool localHostOnly,
     required LogLevel logLevel,
