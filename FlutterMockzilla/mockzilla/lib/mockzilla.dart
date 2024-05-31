1a44af4b09445dff2f44056f4c50ccd7a8c6b31d
JordanH <jordanh@apadmi.com>
2024-05-31 09:16:33 +0100

    chore: export mockzilla config

--- FlutterMockzilla/mockzilla/lib/mockzilla.dart
+++ FlutterMockzilla/mockzilla/lib/mockzilla.dart
@@ -2,5 +2,4 @@ library mockzilla;
 
 export 'src/mockzilla.dart';
 export 'package:mockzilla_platform_interface/mockzilla_platform_interface.dart';
-export 'src/endpoint_configuration_builder_interface.dart';
-export 'src/mockzilla_configuration_builder_interface.dart';
+export 'src/mockzilla_config.dart';
