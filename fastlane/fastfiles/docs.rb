9e9bf1e3855b68c7eb1f8c281e9170e40fd31ddd
chedabob <matt@mattmalone.app>
2024-07-08 21:24:54 +0100

    fix: #168 missing dokka in nav

--- fastlane/fastfiles/docs.rb
+++ fastlane/fastfiles/docs.rb
@@ -4,14 +4,16 @@ lane :generate_docs do
     # Build the page to redirect to the desktop app download site
     sh("cd #{lane_context[:repo_root]}/docs; python -c 'import main; main.update_download_file()'")
 
-    # Build mkdocs
-    sh("cd #{lane_context[:repo_root]}/docs; mkdocs build -d #{output_dir}")
 
     # Generate Kotlin documentation
     gradle(
         tasks: [":dokkaHtmlMultiModule"],
         system_properties: {
-            "docsOutputDirectory" => "#{output_dir}/dokka"
+            "docsOutputDirectory" => "#{lane_context[:repo_root]}/docs/docs/dokka"
         }
     )
+
+    # Build mkdocs
+    sh("cd #{lane_context[:repo_root]}/docs; mkdocs build -d #{output_dir}")
+
 end
