2b3e18f1d0079ee252ee3c1e12a96acb0fdf274d
Sam <samdc@apami.com>
2024-10-21 10:52:53 +0100

    chore: Fix release please

--- fastlane/fastfiles/management-ui.rb
+++ fastlane/fastfiles/management-ui.rb
@@ -2,8 +2,6 @@ require "apadmi_grout"
 
 platform :android do 
 
-    screenshots_output_directory = "#{Apadmi::Grout::GitUtils.git_root}/mockzilla-management-ui/common/src/test/snapshots/images"
-
     lane :management_ui_pull_request do
         gradle(
             tasks: [
