3a04d18c06a07a4a03cba11aaa41dc3f62ac75a9
TomRHandcock <33254448+TomRHandcock@users.noreply.github.com>
2024-12-04 13:22:57 +0000

    fix(mockzilla): Fix version injection
    Update `Project.injectedVersion` to check if 'version' is defined before using it.

--- build-logic/src/main/kotlin/com/apadmi/mockzilla/VersionUtils.kt
+++ build-logic/src/main/kotlin/com/apadmi/mockzilla/VersionUtils.kt
@@ -2,4 +2,5 @@ package com.apadmi.mockzilla
 
 import org.gradle.api.Project
 
-fun Project.injectedVersion() = properties["version"].toString().takeUnless { it.isBlank() || it == "unspecified" }
+fun Project.injectedVersion() = if (project.hasProperty("version")) properties["version"].toString()
+    .takeUnless { it.isBlank() || it == "unspecified" } else null
