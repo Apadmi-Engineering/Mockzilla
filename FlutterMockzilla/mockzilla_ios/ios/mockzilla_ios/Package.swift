25e67ad963a47fcb6ff5f41fa8c0c7e75054ea8e
Tom Ha <tomh@apadmi.com>
2025-01-02 13:02:22 +0000

    fix(flutter): Add `SwiftMockzilla` as dependency in `mockzilla_ios` target

--- FlutterMockzilla/mockzilla_ios/ios/mockzilla_ios/Package.swift
+++ FlutterMockzilla/mockzilla_ios/ios/mockzilla_ios/Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         .target(
             name: "mockzilla_ios",
-            dependencies: [],
+            dependencies: ["SwiftMockzilla"],
             resources: []
         )
     ]
