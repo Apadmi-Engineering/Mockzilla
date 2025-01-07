d56787ad5e66b3c87d09b55197fc8dd4d96cab67
Tom Ha <tomh@apadmi.com>
2025-01-07 16:10:23 +0000

    refactor(flutter): Update `mockzilla` interface
    Update `startMockzilla` method to answer with `MockzillaRuntimeParams`.

--- FlutterMockzilla/mockzilla/lib/src/mockzilla.dart
+++ FlutterMockzilla/mockzilla/lib/src/mockzilla.dart
@@ -2,7 +2,7 @@ import 'package:mockzilla_platform_interface/mockzilla_platform_interface.dart';
 
 abstract class Mockzilla {
   /// Starts the Mockzilla server with the given configuration.
-  static Future<void> startMockzilla(MockzillaConfig config) =>
+  static Future<MockzillaRuntimeParams> startMockzilla(MockzillaConfig config) =>
       MockzillaPlatform.instance.startMockzilla(config);
 
   /// Stops the Mockzilla server.
