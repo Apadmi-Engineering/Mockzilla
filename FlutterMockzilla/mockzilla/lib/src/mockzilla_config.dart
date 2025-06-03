f74f10a92bcea259a85e8fe38ae86e96579b1f1a
Tom Ha <tomh@apadmi.com>
2025-06-03 13:53:48 +0100

    feat(flutter): Implement utility method to `MockzillaConfig`
    Adds utility method to `MockzillaConfig` to allow for adding additional logger instances.

--- FlutterMockzilla/mockzilla/lib/src/mockzilla_config.dart
+++ FlutterMockzilla/mockzilla/lib/src/mockzilla_config.dart
@@ -1,6 +1,12 @@
 import 'package:mockzilla/mockzilla.dart';
 
 extension MockzillaConfigExtensions on MockzillaConfig {
+  /// Returns a copy of this Mockzilla config with the result of [builder]
+  /// added.
   MockzillaConfig addEndpoint(EndpointConfig Function() builder) =>
       copyWith(endpoints: endpoints + [builder()]);
+
+  /// Returns a copy of this Mockzilla config with an additional [logger] added.
+  MockzillaConfig addLogger(MockzillaLogger logger) =>
+      copyWith(loggers: loggers + [logger]);
 }
