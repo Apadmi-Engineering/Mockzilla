d4dcb5c9a43e54254926697abeaeeca7d38d37f3
ethanbfc <ethanbfc@gmail.com>
2025-10-02 15:08:40 +0100

    chore: Update visibility to internal

--- mockzilla-management-ui/mockzilla-mobile-ui/src/commonMain/kotlin/com/apadmi/mockzilla/mobile/ui/utils/Destination.kt
+++ mockzilla-management-ui/mockzilla-mobile-ui/src/commonMain/kotlin/com/apadmi/mockzilla/mobile/ui/utils/Destination.kt
@@ -3,13 +3,13 @@ package com.apadmi.mockzilla.mobile.ui.utils
 import kotlinx.serialization.Serializable
 
 @Serializable
-sealed class Destination {
+internal sealed class Destination {
     @Serializable
-    data object EndpointList : Destination()
+    internal data object EndpointList : Destination()
 
     /**
      * @property key
      */
     @Serializable
-    data class EndpointDetails(val key: String) : Destination()
+    internal data class EndpointDetails(val key: String) : Destination()
 }
