e3b37b1d2599e15ea33afe999d7b1ae1f95a0aaf
ethanbfc <ethanbfc@gmail.com>
2025-10-07 16:58:19 +0100

    feat: Add buttons

---
+++ mockzilla-management-ui/mockzilla-management-ui-common/src/commonMain/kotlin/com/apadmi/mockzilla/ui/ui/common/components/buttons/OutlineButton.kt
@@ -0,0 +1,87 @@
+package com.apadmi.mockzilla.ui.ui.common.components.buttons
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.unit.dp
+import com.apadmi.mockzilla.ui.ui.common.components.PreviewSurface
+import com.apadmi.mockzilla.ui.ui.common.theme.theme_accent
+import com.apadmi.mockzilla.ui.ui.common.theme.theme_success
+import com.apadmi.mockzilla.ui.ui.common.theme.theme_warning
+import com.apadmi.mockzilla_management_ui_common.generated.resources.Res
+import com.apadmi.mockzilla_management_ui_common.generated.resources.lightning_bolt
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun OutlineButton(
+    modifier: Modifier = Modifier,
+    label: String,
+    leadingIcon: Painter? = null,
+    backgroundColor: Color = MaterialTheme.colorScheme.background,
+    outlineColor: Color,
+    contentColor: Color = MaterialTheme.colorScheme.onBackground,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    onClick: () -> Unit
+) = Button(
+    modifier = modifier,
+    onClick = onClick,
+    shape = RoundedCornerShape(8.dp),
+    colors = ButtonColors(
+        containerColor = backgroundColor,
+        contentColor = contentColor,
+        disabledContentColor = backgroundColor,
+        disabledContainerColor = contentColor
+    ),
+    border = BorderStroke(width = 1.dp, color = outlineColor),
+    contentPadding = contentPadding
+) {
+    leadingIcon?.let { icon ->
+        Icon(
+            modifier = Modifier.size(16.dp),
+            painter = icon,
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+    }
+    Text(text = label)
+}
+
+@Preview
+@Composable
+private fun OutlineButtonPreview() = PreviewSurface {
+    Column(
+        modifier = Modifier.padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        OutlineButton(
+            label = "Click me",
+            outlineColor = theme_success,
+            contentColor = theme_success,
+            onClick = {}
+        )
+        OutlineButton(
+            label = "Click me",
+            leadingIcon = painterResource(resource = Res.drawable.lightning_bolt),
+            outlineColor = theme_warning,
+            contentColor = theme_accent,
+            onClick = {}
+        )
+    }
+}
