6b496ab7f620a054b4ee58640ecf6521484bc5dd
ethanbfc <ethanbfc@gmail.com>
2025-10-24 11:39:09 +0100

    chore: Remove icon

--- samples/demo-android/src/main/java/com/apadmi/mockzilla/demo/ui/MainContent.kt
+++ samples/demo-android/src/main/java/com/apadmi/mockzilla/demo/ui/MainContent.kt
@@ -13,11 +13,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -63,12 +60,6 @@ fun MainContent(
                 contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
             ) {
                 Text(text = "Refresh")
-                Spacer(modifier = Modifier.width(2.dp))
-                Icon(
-                    modifier = Modifier.size(20.dp),
-                    imageVector = Icons.Default.Refresh,
-                    contentDescription = null
-                )
             }
         }
         
