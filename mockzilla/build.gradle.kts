cfa8088ac16b95c09bb583cfba4329ee5b2d8094
SamC-Apadmi <55275568+SamC-Apadmi@users.noreply.github.com>
2025-11-14 16:41:16 +0000

    Feature/sync dev (#519)
    * feat!: shorten zeroconf keys (#385)
    
    The shortens the zeroconf keys to align with the spec. This will make old versions of the desktop app incompatible with the new SDK. However the new desktop app will be backward compatible.
    
    * fix: fix yml syntax
    
    * chore(develop): release Kmp libs libraries (#433)
    
    * chore(develop): release Kmp libs libraries
    
    * chore: update versions and changelogs
    
    ---------
    
    Co-authored-by: Sam <samdc@apadmi.com>
    
    * fix: fix mobile ui yml
    
    ---------
    
    Co-authored-by: apadmi-buildserver <87172466+apadmi-buildserver@users.noreply.github.com>

--- mockzilla/build.gradle.kts
+++ mockzilla/build.gradle.kts
@@ -28,7 +28,7 @@ kotlin {
     }
 
     // Managed automatically by release-please PRs
-    version = project.injectedVersion() ?: "2.4.1" // x-release-please-version
+    version = project.injectedVersion() ?: "3.0.0-alpha1" // x-release-please-version
 
     val xcf = XCFramework()
     listOf(
