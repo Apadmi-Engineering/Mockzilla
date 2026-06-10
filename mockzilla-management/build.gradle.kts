156a934aab78edc3e67ee0d0ae06de0c939db750
Olivia Girardot <oliviag@apadmi.com>
2026-06-10 14:54:31 +0100

    fix: Prevent CIO from leaking into js tests

--- mockzilla-management/build.gradle.kts
+++ mockzilla-management/build.gradle.kts
@@ -73,7 +73,6 @@ kotlin {
             api(libs.ktor.server.core)
             implementation(libs.ktor.serialization.kotlinx.json)
             implementation(libs.ktor.client.core)
-            implementation(libs.ktor.client.cio)
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.client.resources)
             implementation(libs.ktor.client.logging)
