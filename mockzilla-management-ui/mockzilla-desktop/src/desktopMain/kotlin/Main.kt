4b30ead57b0933c5b71f7abbc408bdc1c21edc18
SamC-Apadmi <55275568+SamC-Apadmi@users.noreply.github.com>
2026-06-23 14:50:36 +0530

    feat: improve log output for desktop app (#657)

--- mockzilla-management-ui/mockzilla-desktop/src/desktopMain/kotlin/Main.kt
+++ mockzilla-management-ui/mockzilla-desktop/src/desktopMain/kotlin/Main.kt
@@ -2,11 +2,15 @@
 
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.window.*
+
 import com.apadmi.mockzilla.desktop.di.startDesktopMockzillaKoin
 import com.apadmi.mockzilla.desktop.engine.connection.ZeroConfSdkWrapper
 import com.apadmi.mockzilla.desktop.ui.DesktopApp
+import com.apadmi.mockzilla.desktop.utils.DesktopLogWriter
 import com.apadmi.mockzilla.desktop.utils.rememberAppIcon
 import com.apadmi.mockzilla.ui.di.utils.MockzillaUiKoinContext
+
+import co.touchlab.kermit.Logger
 
 fun main() = application {
     val state = rememberWindowState(
@@ -15,6 +19,7 @@ fun main() = application {
         isMinimized = false
     )
 
+    Logger.setLogWriters(logWriters = listOf(DesktopLogWriter()))
     startDesktopMockzillaKoin()
 
     Window(
