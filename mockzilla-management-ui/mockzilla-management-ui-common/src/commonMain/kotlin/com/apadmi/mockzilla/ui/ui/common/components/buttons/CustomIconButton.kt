62f35cda10106853922a8634e71d12ece3d6ded1
Skeletonxf <skeletonxf@gmail.com>
2026-07-14 16:09:23 +0100

    chore: Add previews for icon button component

--- mockzilla-management-ui/mockzilla-management-ui-common/src/commonMain/kotlin/com/apadmi/mockzilla/ui/ui/common/components/buttons/CustomIconButton.kt
+++ mockzilla-management-ui/mockzilla-management-ui-common/src/commonMain/kotlin/com/apadmi/mockzilla/ui/ui/common/components/buttons/CustomIconButton.kt
@@ -1,14 +1,23 @@
 package com.apadmi.mockzilla.ui.ui.common.components.buttons
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.apadmi.mockzilla.lib.InternalMockzillaApi
+import com.apadmi.mockzilla.ui.ui.common.assets.LightningBolt
+import com.apadmi.mockzilla.ui.ui.common.components.PreviewSurface
 import com.apadmi.mockzilla.ui.utils.iconButtonSize
 
 @InternalMockzillaApi
@@ -43,3 +52,30 @@ public fun CustomIconButton(
         }
     }
 }
+
+@Composable
+@Preview
+private fun CustomIconButtonPreview(darkTheme: Boolean = false) = PreviewSurface(darkTheme) {
+    Column(
+        modifier = Modifier.padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        CustomIconButton(
+            onClick = {},
+            imageVector = Icons.LightningBolt,
+            iconTint = LocalContentColor.current,
+            contentDescription = "",
+        )
+        CustomIconButton(
+            onClick = {},
+            imageVector = Icons.LightningBolt,
+            iconTint = LocalContentColor.current,
+            contentDescription = "",
+            enabled = false,
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun CustomIconButtonDarkPreview() = CustomIconButtonPreview(darkTheme = true)
